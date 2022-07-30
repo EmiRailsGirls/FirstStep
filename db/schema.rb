ec0e0e2cc8d74b0559011646fd0677bdde9bc15a
EmiRailsGirls <emi.sasakawa55@gmail.com>
2022-07-30 15:28:22 +0900

    add comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_30_015150) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_30_054831) do
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
+
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_30_015150) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "ideas"
 end
